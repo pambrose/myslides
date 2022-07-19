617e91c3dcdb62cd7d4d5a0ce5e9f18d15aa3c90
Paul Ambrose <pambrose@mac.com>
2022-07-19 10:12:46 -0700

    Add C content

--- src/main/kotlin/C.kt
+++ src/main/kotlin/C.kt
@@ -1,6 +1,5 @@
 import com.kslides.KSlides
 import com.kslides.PlaygroundMode
-import com.kslides.codeSnippet
 import com.kslides.githubRawUrl
 import com.kslides.playground
 import com.kslides.slide.DslSlide
@@ -65,7 +64,7 @@ object C {
             "Static type system with weak enforcement",
             "Architecture-specific language -- not WORA",
             "C source files are compiled to object files",
-            "Write programs and libraries",
+            "Programs and libraries",
           ) {
             style = "font-size:30px; padding-left: 80px"
           }
@@ -79,7 +78,9 @@ object C {
             unorderedList(
               "#include preprocessing directives",
               ".c and .h files",
-              "stdio.h",
+              "Braces, quotes, and ;",
+              "<stdio.h>",
+              "Return success value"
             ) {
               style = "list-style-type:square;"
             }
@@ -106,14 +107,28 @@ object C {
 
       dslSlide {
         content {
-          h2 { +"🚌 Arrays in C" }
+          h2 { +"📮 Working with Hex Addresses" }
+          img { src = "images/hexvalues.png"; width = "900px" }
+        }
+      }
+
+      dslSlide {
+        content {
+          h2 { +"🔬 Variables in Memory" }
+          img { src = "images/basic_types.png"; width = "900px" }
+        }
+      }
+
+      dslSlide {
+        content {
+          h2 { +"🚌 Arrays in C (1)" }
           cSlide("arrays1.c", 500)
         }
       }
 
       dslSlide {
         content {
-          h2 { +"🚌 More Arrays in C" }
+          h2 { +"🚌 Arrays in C (2)" }
           cSlide("arrays2.c", 500)
         }
       }
@@ -121,38 +136,46 @@ object C {
       dslSlide {
         content {
           h2 { +"☝ Pointers in C" }
-          cSlide("pointers1.c")
+          cSlide("pointers1.c", 500)
         }
       }
 
       dslSlide {
         content {
           h2 { +"☝ Pointer Arithmetic in C" }
-          cSlide("pointers2.c")
+          cSlide("pointers2.c", 500)
         }
       }
 
       dslSlide {
         content {
-          h2 { +"🧶 Strings in C" }
-          cSlide("strings1.c")
+          h2 { +"🧶 Strings in C (1)" }
+          cSlide("strings1.c", 500)
         }
       }
 
       dslSlide {
         content {
-          h2 { +"🧶 More Strings in C" }
-          cSlide("strings2.c")
+          h2 { +"🧶 Strings in C (2)" }
+          cSlide("strings2.c", 500)
         }
       }
+
+      dslSlide {
+        content {
+          h2 { +"🧶 Strings in C (3)" }
+          cSlide("strings3.c", 500)
+        }
+      }
+
+
+//      dslSlide {
+//        content {
+//          h2 { +"Stack and Heap" }
+//        }
+//      }
 
       // https://courses.engr.illinois.edu/cs225/sp2022/resources/stack-heap/
-
-      dslSlide {
-        content {
-          h2 { +"Stack and Heap" }
-        }
-      }
 
       val stackCaptions =
         listOf(
@@ -194,356 +217,6 @@ object C {
             h6 { +heapCaptions[i - 1] }
           }
         }
-      dslSlide {
-        content {
-          h2 { +"🚨 Why do we care about Unix?" }
-          unorderedList(
-            "AWS, Google, Azure, Digital Ocean, and more",
-            "Docker and Kubernetes",
-            "Raspberry Pi",
-            "Enterprise Computing",
-            "DevOps",
-            "Data Science",
-          )
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"💪 The Unix Philosophy" }
-          unorderedList(
-            "Write programs that do one thing and do it well -- DOTADIW",
-            "Write programs to work together",
-            "Write programs to handle text streams, because that is a universal interface",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🐚 What is a Unix Shell?" }
-          div {
-            unorderedList(
-              "sh",
-              "csh",
-              "bash",
-              "ksh",
-              "fish",
-              "zsh",
-              "tcsh",
-            ) {
-              style = "font-size:30px"
-            }
-          }
-          br {}
-          a {
-            href = "https://hyperpolyglot.org/unix-shells"
-            target = "_blank"
-            +"👉 Summary of shells"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"✉ Unix Shell Environment Variables" }
-          unorderedList(
-            "env",
-            "echo",
-            "\$PATH / \$HOME / \$USER / \$SHELL",
-            "\$JAVA_HOME / \$CLASSPATH",
-            ".zshrc / .cshrc / .bashrc",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🦺 General Command Structure" }
-          h3 { style = "color: red;"; +"command options args" }
-          unorderedList(
-            "man page",
-            "tldr",
-            "single and double dash options",
-            "which/where/whereis",
-            "args[] in programs",
-          ) {
-            style = "font-size:30px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"📂 File System Commands" }
-          unorderedList(
-            "Files, Directories, and Paths ( / .. . ../ ~ *)",
-            "ls",
-            "touch",
-            "cat/more/less/bat",
-            "mv",
-            "rm",
-            "file",
-            "strings",
-            "pwd",
-            "cd",
-            "pushd/popd",
-            "mkdir/rmdir",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🖊 Unix Editors" }
-          div {
-            unorderedList(
-              "vi/vim/neovim",
-              "emacs",
-              "pico",
-              "nano",
-            ) {
-              style = "font-size:30px; padding-left: 20px"
-            }
-            br {}
-            br {}
-            h3 { style = "color: red;"; +"Choose one (preferably vim) and learn it!!" }
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🛫 I/O Control" }
-          div {
-            unorderedList(
-              "stdin, stdout, and stderr",
-              "Redirecting input/output",
-              "< n> > 2> 2>&1 >>",
-              "/dev/stdin /dev/stdout /dev/stderr",
-            ) {
-              style = "font-size:30px; padding-left: 20px"
-            }
-          }
-          img { width = "800px"; src = "images/diagram-shell-keyboard-terminal.png" }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"📥 Common Patterns - Standard Input" }
-          img { width = "1000px"; src = "images/diagram-input-examples.png" }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"📤 Common Patterns - Standard Output" }
-          img { width = "1000px"; src = "images/diagram-output-examples.png" }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🚿 Pipes" }
-          img { width = "1000px"; src = "images/diagram-cat-sort-uniq-pipeline.png" }
-        }
-      }
-
-      verticalSlides {
-        dslSlide {
-          content {
-            h2 { +"📜 Text Processing" }
-            unorderedList(
-              "wc",
-              "grep",
-              "head/tail",
-              "nl",
-              "fold",
-              "sort",
-              "uniq",
-              "rev",
-              "sed",
-              "tr",
-              "cut",
-            ) {
-              style = "font-size:30px; padding-left: 20px"
-            }
-          }
-        }
-
-        markdownSlide {
-          content {
-            """
-            ## tr examples
-            ```bash
-            head -n 1 ~/samples/data/top100.csv | tr ',' '\n'
-            
-            head -n 1 ~/samples/data/top100.csv | tr ',' '\n' | tr -d '"'
-            
-            echo "Welcome to the shell" | tr 'shell' 'machine'
-            ```
-            """
-          }
-        }
-
-        markdownSlide {
-          content {
-            """
-            ## cut examples
-            ```bash
-            cut -d',' -f 3 ~/samples/data/top100.csv | head
-            
-            cut -d',' -f 1,3 ~/samples/data/top100.csv | head
-            
-            tail -n 5 ~/samples/logs/web-server-logs.txt | cut -c 12-19
-            
-            ps -e | cut -c 26-500
-            ```
-            """
-          }
-        }
-      }
-
-      markdownSlide {
-        content {
-          """
-          ## 🔎 Searching with find
-          ```bash
-            find
-            
-            find . -type f
-            
-            find . -type d
-            
-            find . -name Utils.java
-            
-            find . -name "*.java"
-            
-            find . -name "*.java" -exec wc {} \;
-          ```  
-          """
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🎈 Misc Commands" }
-          unorderedList(
-            "ssh",
-            "sftp",
-            "date/cal",
-            "split",
-            "diff",
-            "compress/uncompress",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"📦 Job/Process Control" }
-          codeSnippet {
-            code = """
-              python3 -m http.server 8080
-            """
-          }
-          unorderedList(
-            "control-z/jobs/fg/bg/&",
-            "top/htop",
-            "ps",
-            "kill",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"📜 Scripts" }
-          codeSnippet {
-            code = """
-              #!/bin/sh
-              echo "🐶 woof 🐶"
-            """
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🧑 User Management" }
-          unorderedList(
-            "w/who",
-            "su",
-            "root",
-            "sudo",
-            "adduser/rmuser",
-            "chmod",
-            "chgrp",
-            "chown",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"🎉 Fun Commands" }
-          unorderedList(
-            "lynx",
-            "say",
-            "fortune",
-            "cowsay",
-            "pbpaste",
-            "banner/figlet",
-            "sl",
-            "cmatrix",
-            "asciiquarium",
-          ) {
-            style = "font-size:30px; padding-left: 20px"
-          }
-        }
-      }
-
-      dslSlide {
-        content {
-          h2 { +"💁 Helpful Links" }
-          a {
-            href = "https://effective-shell.com"
-            target = "_blank"
-            +"👉 Effective Shell"
-          }
-          br {}
-          a {
-            href = "https://www.makeuseof.com/linux-commands-and-alternatives/"
-            target = "_blank"
-            +"👉 8 Classic Commands and Their Modern Alternatives"
-          }
-          br {}
-          a {
-            href = "https://jvns.ca/blog/2022/04/12/a-list-of-new-ish--command-line-tools/"
-            target = "_blank"
-            +"👉 A list of new(ish) command line tools"
-          }
-          br {}
-          a {
-            href = "https://www.tomshardware.com/how-to/getting-to-know-the-linux-filesystem"
-            target = "_blank"
-            +"👉 Getting To Know the Linux Filesystem"
-          }
-        }
-      }
     }
   }
 }
